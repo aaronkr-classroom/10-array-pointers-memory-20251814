f8ea62c59ea8b96fd27e98e9356c10a68c4fea8e
ththfql <145094239+ththfql@users.noreply.github.com>
2025-11-18 17:06:44 +0900

    finish

---
+++ week11/2d_arr_ptr.c
@@ -0,0 +1,73 @@
+﻿// 2d_arr_ptr.c
+#include <stdio.h>
+#include <conio.h> // getch()
+
+#define ROWS 3	 // 행의 개수
+#define COLS 5	 // 열의 개수
+
+// 2D 배열을 출력하는 함수
+void printArr(char arr[ROWS][COLS]) {
+	// printf("배열의 상태:\n");
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+}
+int main(void) {
+
+		char data[ROWS][COLS] = { 0 };
+		char (*p)[COLS];
+		
+
+		p = data; // 포인터 p가 data 배열을 가리키도록 설정
+
+		printf("시작 전:\n");
+		printfArr(data);
+		printf("press any key to continue...\n");
+		getchar();
+
+		(*p)[1] = 3;
+		(*(p + 1))[2] = 4;
+		(*(p + 2))[4] = 5;
+
+		printf("초기 배열의 상태:\n");
+		printfArr(data);
+		printf("press any key to continue...\n");
+		getchar();
+
+		p = data; // 포인터 p를 다시 첫 번째 행으로 초기화
+		p++;      // 포인터 p를 두 번째 행으로 이동
+		(*p)[0] = 7; // 두 번째 행의 첫 번째 요소에 7 할당
+		(*p)[4] = 8;
+		(*(p - 1))[3] = 9; // 첫 번째 행을 수정하기
+
+		printf("두 번째 수정 후 배열의 상태:\n");
+		printfArr(data);
+		printf("press any key to continue...\n");
+		getchar();
+	
+		// 포인터 산술 연산을 사용함
+		char* ptr = &data[0][0]; // 배열의 첫 번째 요소를 가리키는 포인터
+		*(ptr + 6) = 10; // 두 번째 행의 두 번째 요소에 10 할당
+		*(ptr + 10) = 11; // 세 번째 행의 첫 번째 요소에 11 할당
+		*(ptr + 14) = 12; // 마지막 요소에 28 할당
+		// *(ptr + 15) = 100; // 오류! 배열 끝으로 넘어감
+
+		printf("포인터 산술 연산 후 배열의 상태:\n");
+		printfArr(data);
+		printf("press any key to continue...\n");
+		getchar();
+
+		char temp[COLS];
+		for (int i = 0; i < COLS; i++) {
+			temp[i] = data[0][i];
+			data[0][i] = data[2][i];
+			data[2][i] = temp[i];
+		}
+
+		return 0;
+}
---
+++ week11/rubiks_cube.c
@@ -0,0 +1,89 @@
+﻿// rubiks_cube.c
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LENGTH 3
+#define FACES 6
+
+// 전역 변수 선언
+char* colors[FACES] = { 
+	"White","Red", "Blue", "Green","Yellow", "Orange" 
+};
+char letters[FACES] = { 'W','R','B','G','Y','O' };
+int hexColors[FACES] = {
+	0xFFF, 0xF00, 0x0FF, 0x0F0, 0xFA0, 0xFF0
+};
+
+// 3x3x6 배열로 큐브 상태 정의
+int cube[LENGTH][LENGTH][FACES];
+
+// 출력 모드 선택
+enum PrintMode { STRING, LETTER, HEX };
+
+// 큐브 초기화 함수
+void initCube() {
+	for(int i = 0; i < FACES; i++) {
+		for(int j = 0; j < LENGTH; j++) { // 가로 (행)
+			for (int k = 0; k < LENGTH; k++) { // 세로 (열)	
+				cube[i][j][k] = i; // 0~5로 각 면 초기화
+			}
+		}
+	}
+}
+
+// 큐브 출력 함수
+void printCube(enum PrintMode mode) {
+	const char* labels[FACES] = {
+		"Up","Left"," Front","Right","Back", "Down"
+	};
+	for (int i = 0; i < FACES; i++) {
+		printf("%s Face:\n", labels[i]);
+		for (int j = 0; j < LENGTH; j++) {
+			for (int k = 0; k < LENGTH; k++) {
+				switch (mode) {
+				case STRING:
+					printf("%-7s", colors[cube[i][j][k]]); // ----red
+					break;
+				case LETTER:
+
+					break;
+				case HEX:
+					break;
+
+				}
+			}																						
+		}
+	}
+}
+
+// 90도 시계 방향 회전
+void rotateFace(int face) {
+
+}
+
+// 큐브 섞기 함수 (랜덤 회전)
+void scrambleCube(int moves) {
+
+}
+
+// 쉬운 섞기 함수
+void easyScramble() {
+
+}
+
+int main(void) {
+	// 초기화
+	initCube();
+	printf("Initial Cube:\n");
+	printfCube(STRING); // 다른 모드를 선택
+
+	// 섞겨
+	for (int i = 0; i <= 4; i++) {
+		scrambleCube();
+		printfCube(LETTER);
+	}
+
+	return 0;
+}
